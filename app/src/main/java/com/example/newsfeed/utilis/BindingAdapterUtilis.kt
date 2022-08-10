d708159054320580855f994d2936750d2de35143
ellethykhaled-github <ellethykhaled@yahoo.com>
2022-08-10 11:57:37 +0200

    Placeholder modified

--- app/src/main/java/com/example/newsfeed/utilis/BindingAdapterUtilis.kt
+++ app/src/main/java/com/example/newsfeed/utilis/BindingAdapterUtilis.kt
@@ -8,13 +8,13 @@ import com.squareup.picasso.Picasso
 @BindingAdapter(value = ["imageUrl"])
 fun loadImage(imageView: ImageView, url: String?) {
     if (url != null && url.isEmpty()) {
-        imageView.setImageResource(R.drawable.article_image)
+        imageView.setImageResource(R.drawable.placeholder)
         return
     }
 
     Picasso.get()
         .load(url)
-        .placeholder(R.drawable.article_image)
-        .error(R.drawable.article_image)
+        .placeholder(R.drawable.placeholder)
+        .error(R.drawable.placeholder)
         .into(imageView)
 }
